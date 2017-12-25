ac7fcb4d69d84d894425b06d7f9a0bf716b1ea2d
Fatih Taş <fatihtas26@gmail.com>
2017-12-25 03:45:11 +0300

    Fix merge bug

--- src/routes/index.js
+++ src/routes/index.js
@@ -27,7 +27,6 @@ const Routes = () => (
         <AuthRoute path="/register" component={RegisterContainer} />
         <AuthRoute path="/login" component={LoginContainer} />
         <LogoutRoute path="/logout" />
-        <PrivateRoute path="/show/:id" component={ShowContainer} />
         <PrivateRoute path="/upload" component={UploadContainer} />
         <PrivateRoute path="/show/:name" component={ShowContainer} />
         <PrivateRoute path="/friends" component={FriendsContainer} />
